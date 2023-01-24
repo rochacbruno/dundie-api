9a0b1b6ee69d3ec7eeb22b656e4af5cc6626a226
Bruno Rocha <rochacbruno@gmail.com>
2023-01-24 07:54:39 +0000

    add tutorial

---
+++ tutorial/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cargo install mdbook
+cargo install mdbook-catppuccin
+cargo install mdbook-toc
+cargo install mdbook-pagetoc
+cargo install mdbook-mermaid
+cargo install mdbook-admonish
+# mdbook-admonish install --css-dir theme tutorial/
+cargo install mdbook-yml-header
+cargo install mdbook-epub
---
+++ tutorial/src/SUMMARY.md
@@ -0,0 +1,11 @@
+# Summary
+
+[Intro](./intro.md)
+
+# Estrutura
+
+- [Chapter 1](./chapter_1.md)
+- [Chapter 2](./chapter_2.md)
+    - [Nested 1](nested/one.md)
+
+[Final](./final.md)
