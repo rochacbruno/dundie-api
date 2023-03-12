5e0ba051ed73a3ecf850523cb02cab4d4c044fea
Bruno Bereoff <bbereoff@gmail.com>
2023-03-12 18:14:31 -0300

    added: basic cli structure config

---
+++ dundie/cli.py
@@ -0,0 +1,47 @@
+import typer
+from rich.console import Console
+from rich.table import Table
+from sqlmodel import Session, select
+
+from .config import settings
+from .db import engine
+from .models import User
+
+main = typer.Typer(name="dundie CLI", add_completion=False)
+
+
+@main.command()
+def shell() -> None:  # pyright: ignore
+    """Opens interactive shell"""
+    _vars = {
+        "settings": settings,
+        "engine": engine,
+        "select": select,
+        "session": Session(engine),
+        "User": User,
+    }
+    typer.echo(f"Auto imports: {list(_vars.keys())}")
+    try:
+        from IPython import start_ipython
+
+        start_ipython(argv=["--ipython-dir=/tmp", "--no-banner"], user_ns=_vars)  # type: ignore
+    except ImportError:
+        import code
+
+        code.InteractiveConsole(_vars).interact()
+
+
+@main.command()
+def user_list() -> None:  # pyright: ignore
+    """Lists all users"""
+    table = Table(title="dundie users")
+    fields = ["name", "username", "dept", "email", "currency"]
+    for header in fields:
+        table.add_column(header, style="magenta")
+
+    with Session(engine) as session:
+        users = session.exec(select(User))
+        for user in users:
+            table.add_row(*[getattr(user, field) for field in fields])
+
+    Console().print(table)
