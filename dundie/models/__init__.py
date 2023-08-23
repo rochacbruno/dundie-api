bbc70dce615ec85f15b3b04bc11e279ddf7d8fa2
Bruno Bereoff <bbereoff@gmail.com>
2023-08-23 20:24:47 -0300

    added: transaction and balance structures; added: migrations

--- dundie/models/__init__.py
+++ dundie/models/__init__.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel
 
+from .transaction import Transaction, Balance
 from .user import User
 
-__all__ = ["User", "SQLModel"]
+__all__ = ["User", "SQLModel", "Transaction", "Balance"]
--- dundie/models/user.py
+++ dundie/models/user.py
@@ -1,11 +1,14 @@
 """User related data models"""
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
 from fastapi import HTTPException, status
 from pydantic import BaseModel, root_validator
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 
 from dundie.security import HashedPassword, get_password_hash
+
+if TYPE_CHECKING:
+    from dundie.models.transaction import Transaction, Balance
 
 
 class User(SQLModel, table=True):
@@ -20,6 +23,28 @@ class User(SQLModel, table=True):
     name: str = Field(nullable=False)
     dept: str = Field(nullable=False)
     currency: str = Field(nullable=False)
+
+    # Populates a `.user` on `Transaction`
+    incomes: Optional[list["Transaction"]] = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={"primaryjoin": 'User.id == Transaction.user_id'},
+    )
+    # Populates a `.from_user` on `Transaction`
+    expenses: Optional[list["Transaction"]] = Relationship(
+        back_populates="from_user",
+        sa_relationship_kwargs={"primaryjoin": 'User.id == Transaction.from_id'},
+    )
+    # Populates a `.user` on `Balance`
+    _balance: Optional["Balance"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={"lazy": "dynamic"}
+    )
+
+    @property
+    def balance(self) -> int:
+        """Returns the current balance of the user"""
+        if (user_balance := self._balance.first()) is not None:  # pyright: ignore
+            return user_balance.value
+        return 0
 
     @property
     def superuser(self) -> bool:
