49acc7f3ea1a52376a9e55fc56de20d46feebaa1
Bruno Bereoff <bbereoff@gmail.com>
2023-08-27 20:39:33 -0300

    added: transaction router to module __init__

--- dundie/routes/__init__.py
+++ dundie/routes/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .auth import router as auth_router
 from .user import router as user_router
+from .transaction import router as transaction_router
 
 main_router = APIRouter()
 
 main_router.include_router(user_router, prefix="/user", tags=["user"])
+main_router.include_router(transaction_router, prefix="/transaction", tags=["transaction"])
 main_router.include_router(auth_router, tags=["auth"])
