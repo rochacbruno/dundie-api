70e30354ae9833914a91cbc4b3ff7074ff100bd8
Bruno Bereoff <bbereoff@gmail.com>
2023-08-27 22:57:39 -0300

    added: condional balance info serializer into list_users and get_user endpoints

--- dundie/routes/user.py
+++ dundie/routes/user.py
@@ -1,11 +1,14 @@
 from typing import List
 
-from fastapi import APIRouter, Body, BackgroundTasks
+from fastapi import APIRouter, BackgroundTasks, Body
+from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import parse_obj_as
 from sqlalchemy.exc import IntegrityError
 from sqlmodel import Session, select
 
-from dundie.auth import AuthenticatedUser, CanChangeUserPassword, SuperUser
+from dundie.auth import AuthenticatedUser, CanChangeUserPassword, ShowBalanceField, SuperUser
 from dundie.db import ActiveSession
 from dundie.models.user import (
     User,
@@ -13,6 +16,7 @@ from dundie.models.user import (
     UserProfilePatchRequest,
     UserRequest,
     UserResponse,
+    UserResponseWithBalance,
 )
 from dundie.queue import queue
 from dundie.tasks.user import try_to_send_pwd_reset_email
@@ -20,18 +24,48 @@ from dundie.tasks.user import try_to_send_pwd_reset_email
 router = APIRouter()
 
 
-@router.get("/", response_model=List[UserResponse], dependencies=[AuthenticatedUser])
-async def list_users(*, session: Session = ActiveSession):
+@router.get("/",
+            response_model=List[UserResponse] | List[UserResponseWithBalance],
+            response_model_exclude_unset=True,
+            # dependencies=[AuthenticatedUser],
+            )
+async def list_users(
+    *,
+    session: Session = ActiveSession,
+    show_balance_field: bool = ShowBalanceField,
+):
     """List all users."""
     users = session.exec(select(User)).all()
+    # TODO: pagination
+
+    if show_balance_field:
+        users_with_balance = parse_obj_as(List[UserResponseWithBalance], users)
+        return JSONResponse(jsonable_encoder(users_with_balance))
+
     return users
 
 
-@router.get("/{username}/", response_model=UserResponse)
-async def get_user_by_username(*, session: Session = ActiveSession, username: str):
+@router.get(
+    "/{username}/",
+    response_model=UserResponse | UserResponseWithBalance,
+    response_model_exclude_unset=True
+)
+async def get_user_by_username(
+    *,
+    session: Session = ActiveSession,
+    show_balance_field: bool = ShowBalanceField,
+    username: str
+):
     """Get single user by username"""
     query = select(User).where(User.username == username)
     user = session.exec(query).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if show_balance_field:
+        user_with_balance = parse_obj_as(UserResponseWithBalance, user)
+        return JSONResponse(jsonable_encoder(user_with_balance))
     return user
 
 
